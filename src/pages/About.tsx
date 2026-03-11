3ea5edd4b7b5b4cef0522fb427e65c834599e880
Theoxenos <64856229+theoxenos@users.noreply.github.com>
2026-03-11 15:01:08 +0100

    wip

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,5 @@
+import {Carousel, Col, Row} from "react-bootstrap";
+
 const About = () => {
     return (
         <div className="mt-5 text-nord-6">
@@ -5,6 +7,26 @@ const About = () => {
             <p className="mt-3">
                 This is the about page.
             </p>
+            <Row xs={12} md={6} lg={4} className="justify-content-center">
+                <Col>
+                    <Carousel variant="dark">
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://placehold.co/200x400"
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://placehold.co/200x400"
+                                alt="Second slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
         </div>
     );
 }
